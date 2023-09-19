5c69cb8e2bf91c888f1588fa9109d4fa67f05533
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-09-19 16:43:48 +0900

    Initial commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+xcodebuild -project AppleLocalization/AppleLocalization.xcodeproj \
+   -scheme AppleLocalization \
+   -derivedDataPath ./DerivedData \
+   -sdk iphoneos build-for-testing
+
+(cd ./DerivedData/Build/Products/ && \
+  zip -r MyTests.zip Debug-iphoneos 
+AppleLocalization_AppleLocalization_iphoneos16.4-arm64.xctestrun)
