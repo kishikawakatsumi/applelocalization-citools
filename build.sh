20ad9bc46d181bf421986a0f0673eaaaa612956a
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-09-22 20:32:07 +0900

    Tweaks

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ xcodebuild -project AppleLocalization/AppleLocalization.xcodeproj \
    -sdk iphoneos build-for-testing
 
 (cd ./DerivedData/Build/Products/ && \
-  zip -r MyTests.zip Debug-iphoneos 
-AppleLocalization_AppleLocalization_iphoneos16.4-arm64.xctestrun)
+  zip -r MyTests.zip Debug-iphoneos AppleLocalization_AppleLocalization_iphoneos16.4-arm64.xctestrun)
